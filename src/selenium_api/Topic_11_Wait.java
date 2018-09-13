09b26db7aaa2af709706dc6a8d1a9c24de7f5c8f
ThaoNT <thaonguyen30121995@gmail.com>
2018-09-13 23:40:45 +0700

    Update TC_03

--- src/selenium_api/Topic_11_Wait.java
+++ src/selenium_api/Topic_11_Wait.java
@@ -1,16 +1,22 @@
 package selenium_api;
 
 import org.testng.annotations.Test;
+
+import com.google.common.base.Function;
+
 import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 
@@ -38,7 +44,7 @@ public class Topic_11_Wait {
 	Assert.assertEquals(resulttext, "Hello World!");
 	
   }
-  @Test
+  //@Test
   public void TC_02() {
 	  driver.get("http://demos.telerik.com/aspnet-ajax/ajaxloadingpanel/functionality/explicit-show-hide/defaultcs.aspx");
 	  
@@ -66,6 +72,35 @@ public class Topic_11_Wait {
 	  
 	  Assert.assertEquals(SelectedDate, "Wednesday, September 05, 2018");
   
+  }
+  
+  @Test
+  public void TC_03() {
+	  driver.get("https://daominhdam.github.io/fluent-wait/");
+	  
+	  WebElement countdount = driver.findElement(By.xpath(".//*[@id='javascript_countdown_time']"));
+  
+	  wait.until(ExpectedConditions.visibilityOf(countdount));
+
+	// Khởi tạo Fluent wait
+	new FluentWait<WebElement>(countdount)
+	           // Tổng time wait là 15s
+	           .withTimeout(15, TimeUnit.SECONDS)
+	            // Tần số mỗi 1s check 1 lần
+	            .pollingEvery(1, TimeUnit.SECONDS)
+	           // Nếu gặp exception là find ko thấy element sẽ bỏ  qua
+	            .ignoring(NoSuchElementException.class)
+	            // Kiểm tra điều kiện
+	            .until(new Function<WebElement, Boolean>() {
+	                public Boolean apply(WebElement element) {
+	                           // Kiểm tra điều kiện countdount = 00
+	                           boolean flag =  element.getText().endsWith("00");
+	                           System.out.println("Time = " +  element.getText());
+	                           // return giá trị cho function apply
+	                           return flag;
+	                      }
+	               });
+	  
   }
   @AfterClass
   public void afterClass() {
